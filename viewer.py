106820aae3cd680c65144b493d6b7af7f3ef65b1
Jozef Vesely <vesely@gjh.sk>
2014-02-18 13:57:02 +0100

    update viewer

--- viewer.py
+++ viewer.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 
 if __name__ == "__main__":
-    
     import sys, ae
     from matplotlib.pyplot import *
-    figure(figsize=(8,4))
-    subplots_adjust(0.12,0.15,0.98,0.98)
-    [ ae.open(fname).plot(label=fname) for  fname in sys.argv[1:]]
-    ae.xpan()
-    legend()
-    xlabel("time [s]")
-    ylabel("amplitude [V]")
-    grid()
+    for fname in sys.argv[1:]:
+        x = ae.open(fname)
+        print x
+        figure(fname, figsize=(8,4))
+        subplots_adjust(0.10,0.10,0.98,0.95)
+        #subplots_adjust(0.12,0.15,0.98,0.98)
+
+        for ch in range(x.channels)[::-1]:
+            x.plot(channel=ch, label="ch#{}".format(ch))
+        ae.xpan()
+        legend()
+        grid()
+        #xlabel("time [s]")
+        #ylabel("amplitude [V]")
     show()
