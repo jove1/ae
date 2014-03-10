77681736e9f8ae2604d6dda1de5e40e9e268489d
Jozef Vesely <vesely@gjh.sk>
2014-03-10 19:35:12 +0100

    add support for crosscompilation using mingw-w64

---
+++ cross.py
@@ -0,0 +1,304 @@
+
+
+
+wininst_table = {
+        "win32-2.5": "wininst-7.1.exe",
+    "win-amd64-2.5": "wininst-8.0.exe",
+        "win32-2.6": "wininst-9.0.exe",
+    "win-amd64-2.6": "wininst-9.0-amd64.exe",
+        "win32-2.7": "wininst-9.0.exe",
+    "win-amd64-2.7": "wininst-9.0-amd64.exe",
+}
+
+
+msvct_table = {
+        "2.5": "msvcr71",
+        "2.6": "msvcr90",
+        "2.7": "msvcr90",
+}
+
+def make_specs(compiler, msvcr, msvcrt_version=None, moldname=None):
+    import subprocess
+    specs = subprocess.Popen([compiler, "-dumpspecs"], stdout=subprocess.PIPE).communicate()[0].decode('ascii')
+
+    import re
+    newspecs = []
+    m = re.search(r"(\*cpp:(?:\n|\r\n))(.*?)(\n\n|\r\n\r\n)", specs, re.DOTALL)
+    if m and msvcrt_version is not None:
+        a,b,c = m.groups()
+        b += " -D__MSVCRT_VERSION__=0x%03i0" % (msvcrt_version,)
+        newspecs.extend([a,b,c])
+    m = re.search(r"(\*libgcc:(?:\n|\r\n))(.*?)(\n\n|\r\n\r\n)", specs, re.DOTALL)
+    if m:
+        a,b,c = m.groups()
+        b = b.replace("msvcrt",msvcr)
+        if moldname is not None:
+            b = b.replace("moldname",moldname)
+        newspecs.extend([a,b,c])
+    return "".join(newspecs)
+
+import sys, os, string
+
+from distutils.command.build_ext import build_ext 
+from distutils.command.build import build
+from distutils.command.bdist_wininst import bdist_wininst
+from distutils import log
+from distutils.dir_util import remove_tree
+from distutils.errors import DistutilsOptionError, DistutilsFileError
+
+
+class cross_build(build):
+    user_options = build.user_options + [
+        ('cross-dir=', None,
+         "Python headers and libraries for cross compilation"),
+        ('cross-ver=', None,
+         "version for cross compilation"),
+        ('cross-compiler=', None,
+         "compiler for cross compilation"),
+    ]
+
+    def initialize_options(self):
+        build.initialize_options(self)
+        self.cross_dir = None
+        self.cross_ver = None
+        self.cross_compiler = None
+    
+    def finalize_options(self):
+        if self.cross_dir is None:
+            build.finalize_options(self)
+        else: 
+            import re, os.path
+            regexp = ".*\.(win32|win-amd64)-((?:[0-9])\.(?:[0-9]))"
+            plat, ver = re.match(regexp, self.cross_dir).groups()
+
+            if self.plat_name is None:
+                self.plat_name = plat
+
+            if self.cross_ver is None:
+                self.cross_ver = ver
+
+            if self.build_purelib is None:
+                self.build_purelib = os.path.join(self.build_base,
+                                                  'lib.{}-{}'.format(self.plat_name, self.cross_ver))
+            if self.build_platlib is None:
+                self.build_platlib = os.path.join(self.build_base,
+                                                  'lib.{}-{}'.format(self.plat_name, self.cross_ver))
+
+            if self.build_lib is None:
+                if self.distribution.ext_modules:
+                    self.build_lib = self.build_platlib
+                else:
+                    self.build_lib = self.build_purelib
+
+            if self.build_temp is None:
+                self.build_temp = os.path.join(self.build_base,
+                                               'temp.{}-{}'.format(self.plat_name, self.cross_ver))
+            if self.build_scripts is None:
+                self.build_scripts = os.path.join(self.build_base,
+                                                  'scripts-{}'.format(self.cross_ver) )
+
+class cross_build_ext(build_ext):
+    user_options = build_ext.user_options + [
+        ('cross-dir=', None,
+         "Python headers and libraries for cross compilation"),
+        ('cross-ver=', None,
+         "version for cross compilation"),
+        ('cross-compiler=', None,
+         "compiler for cross compilation"),
+    ]
+
+    def initialize_options(self):
+        build_ext.initialize_options(self)
+        self.cross_dir = None
+        self.cross_ver = None
+        self.cross_compiler = None
+ 
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        self.set_undefined_options('build',
+                ('cross_dir', 'cross_dir'),
+                ('cross_ver', 'cross_ver'),
+                ('cross_compiler', 'cross_compiler'),
+        )
+
+        if self.cross_compiler is None:
+            self.cross_compiler = { "win32": "i686-w64-mingw32-gcc",
+                                    "win-amd64": "x86_64-w64-mingw32-gcc" }.get(self.plat_name)
+
+    def run(self):
+        if not self.extensions:
+            return
+
+        if self.distribution.has_c_libraries():
+            build_clib = self.get_finalized_command('build_clib')
+            self.libraries.extend(build_clib.get_library_names() or [])
+            self.library_dirs.append(build_clib.build_clib)
+
+        from distutils.unixccompiler import UnixCCompiler
+
+        self.compiler = UnixCCompiler(verbose=self.verbose,
+                                      dry_run=self.dry_run,
+                                      force=self.force)
+        
+        self.compiler.shared_lib_extension = ".pyd"
+
+        if self.libraries is not None:
+            self.compiler.set_libraries(self.libraries)
+
+        if self.library_dirs is not None:
+            self.compiler.set_library_dirs(self.library_dirs)
+
+       
+        python_lib = "python"+self.cross_ver.replace(".","")
+
+        import os.path, subprocess
+        if not os.path.exists( os.path.join(self.cross_dir, python_lib+".def") ):
+            log.info("making def for %s in %s", python_lib, self.cross_dir)
+            subprocess.check_call(["gendef", python_lib+".dll"], cwd=self.cross_dir)
+       
+        python_lib_fname = self.compiler.static_lib_format % (python_lib, self.compiler.static_lib_extension )
+        if not os.path.exists( os.path.join(self.cross_dir, python_lib_fname) ):
+            log.info("making link library %s for %s in %s", python_lib_fname, python_lib, self.cross_dir)
+            print self.cross_compiler.replace("gcc","dlltool")
+            subprocess.check_call([self.cross_compiler.replace("gcc","dlltool"), 
+                "--dllname", python_lib+".dll",
+                "--def", python_lib+".def", 
+                "--output-lib", python_lib_fname], cwd=self.cross_dir)
+
+        specs_fname = os.path.join(self.cross_dir, "compiler.specs")
+        if not os.path.exists(specs_fname):
+            log.info("making compiler specs %s", specs_fname)
+            msvcr = msvct_table.get(self.cross_ver)
+            newspecs = make_specs(self.cross_compiler, msvcr, int(msvcr[-2:]))
+            fh = open(specs_fname, "w")
+            fh.write(newspecs)
+            fh.close()
+
+        self.compiler.set_executables(
+            compiler_so="{} -specs={}".format(self.cross_compiler, specs_fname),
+            linker_so="{} -specs={} -static-libgcc -shared".format(self.cross_compiler, specs_fname),
+        )
+
+
+        if "win-amd64" == self.plat_name:
+            self.compiler.define_macro("MS_WIN64")
+        
+        self.compiler.add_library(python_lib)
+        self.compiler.add_library_dir(self.cross_dir)
+        self.compiler.add_include_dir(os.path.join(self.cross_dir, 'include'))
+
+        # Now actually compile and link everything.
+        self.build_extensions()
+
+class cross_bdist_wininst(bdist_wininst):
+    user_options = build_ext.user_options + [
+        ('wininst-exe=', None,
+         "location of wininst.exe")
+    ]
+
+    def initialize_options(self):
+        bdist_wininst.initialize_options(self)
+        self.wininst_exe = None
+
+    def finalize_options(self):
+        self.skip_build = True
+        self.set_undefined_options('build', ('cross_ver', 'target_version'))
+        bdist_wininst.finalize_options(self)
+
+
+        if self.wininst_exe is None:
+            import distutils.command.bdist_wininst
+            directory = os.path.dirname(distutils.command.bdist_wininst.__file__)
+            wininst = wininst_table.get( self.plat_name+"-"+self.target_version )
+            
+            self.wininst_exe = os.path.join(directory, wininst)
+
+    def run (self):
+        if not self.skip_build:
+            self.run_command('build')
+
+        install = self.reinitialize_command('install', reinit_subcommands=1)
+        install.root = self.bdist_dir
+        install.skip_build = self.skip_build
+        install.warn_dir = 0
+        install.plat_name = self.plat_name
+
+        install_lib = self.reinitialize_command('install_lib')
+        # we do not want to include pyc or pyo files
+        install_lib.compile = 0
+        install_lib.optimize = 0
+
+        if self.distribution.has_ext_modules():
+            # If we are building an installer for a Python version other
+            # than the one we are currently running, then we need to ensure
+            # our build_lib reflects the other Python version rather than ours.
+            # Note that for target_version!=sys.version, we must have skipped the
+            # build step, so there is no issue with enforcing the build of this
+            # version.
+            target_version = self.target_version
+            if not target_version:
+                assert self.skip_build, "Should have already checked this"
+                target_version = sys.version[0:3]
+            plat_specifier = ".%s-%s" % (self.plat_name, target_version)
+            build = self.get_finalized_command('build')
+            build.build_lib = os.path.join(build.build_base,
+                                           'lib' + plat_specifier)
+
+        # Use a custom scheme for the zip-file, because we have to decide
+        # at installation time which scheme to use.
+        for key in ('purelib', 'platlib', 'headers', 'scripts', 'data'):
+            value = string.upper(key)
+            if key == 'headers':
+                value = value + '/Include/$dist_name'
+            setattr(install,
+                    'install_' + key,
+                    value)
+
+        log.info("installing to %s", self.bdist_dir)
+        install.ensure_finalized()
+
+        # avoid warning of 'install_lib' about installing
+        # into a directory not in sys.path
+        sys.path.insert(0, os.path.join(self.bdist_dir, 'PURELIB'))
+
+        install.run()
+
+        del sys.path[0]
+
+        # And make an archive relative to the root of the
+        # pseudo-installation tree.
+        from tempfile import mktemp
+        archive_basename = mktemp()
+        fullname = self.distribution.get_fullname()
+        arcname = self.make_archive(archive_basename, "zip",
+                                    root_dir=self.bdist_dir)
+        # create an exe containing the zip-file
+        self.create_exe(arcname, fullname, self.bitmap)
+        if self.distribution.has_ext_modules():
+            pyversion = self.target_version
+        else:
+            pyversion = 'any'
+        self.distribution.dist_files.append(('bdist_wininst', pyversion,
+                                             self.get_installer_filename(fullname)))
+        # remove the zip-file again
+        log.debug("removing temporary file '%s'", arcname)
+        os.remove(arcname)
+
+        if not self.keep_temp:
+            remove_tree(self.bdist_dir, dry_run=self.dry_run)
+
+    def get_exe_bytes (self):
+        try:
+            f = open(self.wininst_exe, "rb")
+        except IOError, msg:
+            raise DistutilsFileError, str(msg) + ', please install the python%s-dev package' % sys.version[:3]
+        try:
+            return f.read()
+        finally:
+            f.close()
+
+cross_cmdclass = {
+    'build': cross_build,
+    'build_ext': cross_build_ext,
+    'bdist_wininst': cross_bdist_wininst,
+}
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
-from numpy.distutils.core import setup, Extension
+from distutils.core import setup, Extension
+import os.path, sys
+import numpy
+
+numpy_include = [os.path.join(os.path.dirname(numpy.__file__), "core", "include")]
 
 extra_scripts = []
 extra_options = {}
-import sys
+cmdclass = {}
 if "bdist_wininst" in sys.argv:
     install_script = "ae_post_install.py"
     extra_scripts.append(install_script)
@@ -13,7 +17,12 @@ if "bdist_wininst" in sys.argv:
         "install_script": install_script,
         "user_access_control": "auto",
     }
-    
+
+if "cross" in sys.argv:
+    sys.argv.remove("cross")
+    from cross import cross_cmdclass as cmdclass
+    numpy_include = []
+
 setup(name='ae',
       version='0.2',
       description='Python accoustic emission tools',
@@ -25,9 +34,11 @@ setup(name='ae',
       ext_modules=[
           Extension('ae.event_detector', 
               define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+              include_dirs=numpy_include,
               sources=['ae/event_detector.c'],
               ),
           ],
       requires=['numpy'],
       options=extra_options,
+      cmdclass=cmdclass,
       )
